d8e6a2822af4ea6d19e312499d93059db2bfe542
YancyReyes <77408187+YancyReyes@users.noreply.github.com>
2021-01-14 02:20:31 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.yancyoliver;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {2,5,42,2,2};
+
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
